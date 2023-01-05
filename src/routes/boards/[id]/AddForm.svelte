4ed52668d6564d8242f79ef00bdef7b02d2b7457
Nicola Biada <nicola@biada.it>
2023-01-05 09:15:20 +0100

    Prettier

--- src/routes/boards/[id]/AddForm.svelte
+++ src/routes/boards/[id]/AddForm.svelte
@@ -1,86 +1,97 @@
 <script>
-  import { createEventDispatcher, onMount } from 'svelte'
+	import { createEventDispatcher, onMount } from 'svelte';
 
-  let classes
+	let classes;
 
-  export {classes as class}
-  export let placeholder
-  export let action = 'Add'
+	export { classes as class };
+	export let placeholder;
+	export let action = 'Add';
 
-  const dispatch = createEventDispatcher()
-  let input
-  let value = ''
+	const dispatch = createEventDispatcher();
+	let input;
+	let value = '';
 
-  onMount(() => {
-    input.focus()
-  })
+	onMount(() => {
+		input.focus();
+	});
 
-  function submit() {
-    dispatch('submit', value)
-    value = ''
-  }
+	function submit() {
+		dispatch('submit', value);
+		value = '';
+	}
 
-  function close() {
-    dispatch('close')
-    value = ''
-  }
+	function close() {
+		dispatch('close');
+		value = '';
+	}
 
-  function keydown(event) {
-    if (event.key == 'Escape') {
-      close()
-      event.preventDefault()
-    }
-  }
+	function keydown(event) {
+		if (event.key == 'Escape') {
+			close();
+			event.preventDefault();
+		}
+	}
 </script>
 
 <form on:submit|preventDefault={submit} class={classes}>
-  <input bind:value bind:this={input} {placeholder} on:keydown={keydown} required/>
+	<input bind:value bind:this={input} {placeholder} on:keydown={keydown} required />
 
-  <div class="actions">
-    <button class="submit" type="submit">{action}</button>
-    <button class="close" type="button" on:click={close}>
-      <svg height=20 xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-      </svg>
-    </button>
-  </div>
+	<div class="actions">
+		<button class="submit" type="submit">{action}</button>
+		<button class="close" type="button" on:click={close}>
+			<svg
+				height="20"
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke="currentColor"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					stroke-width="2"
+					d="M6 18L18 6M6 6l12 12"
+				/>
+			</svg>
+		</button>
+	</div>
 </form>
 
 <style>
-  form {
-    display: flex;
-    flex-direction: column;
-  }
+	form {
+		display: flex;
+		flex-direction: column;
+	}
 
-  form .actions {
-    display: flex;
-    margin: 0.5rem 0 0;
-  }
+	form .actions {
+		display: flex;
+		margin: 0.5rem 0 0;
+	}
 
-  button {
-    display: flex;
-    font-size: 1rem;
-    align-items: center;
-    width: 100%;
-    color: white;
-  }
+	button {
+		display: flex;
+		font-size: 1rem;
+		align-items: center;
+		width: 100%;
+		color: white;
+	}
 
-  button.submit {
-    background-color: #4dc7dc;
-    padding: 0.4rem;
-    border-radius: 3px;
-    text-align: center;
-    display: inline-block;
-    width: 100px;
-  }
+	button.submit {
+		background-color: #4dc7dc;
+		padding: 0.4rem;
+		border-radius: 3px;
+		text-align: center;
+		display: inline-block;
+		width: 100px;
+	}
 
-  button.close {
-    color: #444;
-    max-width: 30px;
-    text-align: center;
-  }
+	button.close {
+		color: #444;
+		max-width: 30px;
+		text-align: center;
+	}
 
-  button svg {
-    height: 1.2rem;
-  }
+	button svg {
+		height: 1.2rem;
+	}
 </style>
